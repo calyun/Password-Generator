cdd0cf9c5b16f49dffea9d0ea1050d9951ca4f83
calyun <cmyounk@gmail.com>
2021-06-13 18:37:50 -0400

    Added password length and character type selection

--- assests/script.js
+++ assests/script.js
@@ -3,8 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  // Setting Password Length
+
+  var passwordLength = window.prompt("How long should you password be? (8-128 Characters)")
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    console.log("input not valid");
+    return;
+  } else {
+    console.log(passwordLength)
+    console.log("its gucci")
+    //Setting included character types
+  var charactertype = window.prompt("Please type all character types you would like to include. (Lowercase, Uppercase, Numeric, Special)")
+  console.log(charactertype);
+  } if (charactertype == "Lowercase", "Uppercase", "Numeric", "Special") {
+    console.log("s'all goodman");
+  } else {
+    console.log("sus");
+    return;
+  }
 
   passwordText.value = password;
 
