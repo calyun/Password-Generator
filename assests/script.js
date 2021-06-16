e9adf7344358bc25c165ac81d000d945613fe5ca
calyun <cmyounk@gmail.com>
2021-06-15 21:55:46 -0400

    Everything Works! Fixed Password Generator

--- assests/script.js
+++ assests/script.js
@@ -2,43 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-// function writePassword() {
-//   // var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
-//   // Setting Password Length
 
-
-//     //Setting included character types
-
-//   var passwordArray = ["a", "b", "c", "A", "B", "C", "1", "2", "3", "!", "@", "#"]
-//   // var lcArray = ["a", "b", "c"]
-//   // var ucArray = ["A", "B", "C"]
-//   // var nArray = ["1", "2", "3"]
-//   // var sArray = ["!", "@", "#"]
-//   console.log(passwordArray)
-
-//   
-//   // if (lowerCase == "Yes") {
-//   //   passwordArray + lcArray
-//   // }
-//   
-//   // if (upperCase == "Yes") {
-//   //   passwordArray + ucArray
-//   // }
-//   
-//   // if (numeric == "Yes") {
-//   //   passwordArray + nArray
-//   // }
-//   var special = window.prompt("Would you like to include special? (Yes or No)")
-//   // if (special == "Yes") {
-//   //   passwordArray + sArray
-//   // }
-//   // passwordText.value = password;
-
-//   
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 //function to create password
 function writePassword() {
   //user input for password length
@@ -50,11 +16,13 @@ function writePassword() {
     console.log(passwordLength)
     console.log("its gucci")
   }
+
   //lets user pick which characters to include  
   var lowerCase = window.prompt("Would you like to include lowercase? (Yes or No)")
   var upperCase = window.prompt("Would you like to include uppercase? (Yes or No)")
   var numeric = window.prompt("Would you like to include numeric? (Yes or No)")
   var special = window.prompt("Would you like to include special? (Yes or No)")
+
   //Checks to see if at least one character type is selected
   if ((lowerCase !== "Yes")&&(upperCase !== "Yes")&&(numeric !== "Yes")&&(special !== "Yes")){
   window.alert("Please select at least one character type.")
@@ -78,19 +46,24 @@ function writePassword() {
   if (special == "Yes") {
     sp.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")")
   };
-  var newChars = lC.concat(uC, nu, sp);
-  console.log(newChars);
-  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var chars = lC.concat(uC, nu, sp);
+  console.log(chars);
+  console.log(chars.length);
 
   //for loop to randomly generate password
-  for (i=0;i<(passwordLength);i++) {
-    var c = Math.floor(Math.random()*chars.length + 1);
-    actualPassword += chars.charAt(c);
-  }
+  for (var i = 0; i < passwordLength; i++) {
+    actualPassword += chars[Math.floor(Math.random() * chars.length)]
+  };
+
+  //just checkin thangs
   console.log(actualPassword);
   console.log(actualPassword.length);
-  passwordText.innerHTML = actualPassword
+
+  //plus generated password into html
+  passwordText.innerHTML = actualPassword;
 
   return actualPassword;
   
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
